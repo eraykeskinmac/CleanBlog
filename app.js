3f074d2f1c97048893f3092a6e4bb66505002ad4
Mustafa Eray Keskin <meraykeskin@gmail.com>
2021-05-27 18:50:59 +0300

    done

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const blog = {
+    id: 1,
+    title: 'Blog title',
+    description: 'Blog description',
+  };
+  res.send(blog);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
